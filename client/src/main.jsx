22b740e4446b33fd59a886b0f20276e8ee88b000
ALEXANDRE MORO <alexandre.moro@live.fr>
2024-05-14 09:46:36 +0200

    first commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -25,7 +25,17 @@ const router = createBrowserRouter([
       {
         path: "/cupcakes",
         element: <CupcakeList />,
-        // Step 1: load data here
+        loader: async () => {
+          try {
+            const res = await fetch('http://localhost:3310/api/cupcakes')
+            const data = await res.json()
+            console.info(data);
+            return(data)
+          } catch(e) {
+            console.error(e);
+            return[]
+          }
+        }
       },
     ],
   },
