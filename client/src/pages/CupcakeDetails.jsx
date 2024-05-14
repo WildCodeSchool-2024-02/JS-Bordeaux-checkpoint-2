67fb89fd3ac5af05b03daae7a2d307ead9ebd75c
sylvain.lafaye <sylvain-lafaye@hotmail.fr>
2024-05-14 15:48:02 +0200

    finished step 6

---
+++ client/src/pages/CupcakeDetails.jsx
@@ -0,0 +1,14 @@
+
+import { useLoaderData } from "react-router-dom"
+import Cupcake from "../components/Cupcake"
+
+function CupcakeDetails () {
+    const data = useLoaderData();
+
+    return(
+        <Cupcake data={data}/>
+    )
+
+}
+
+export default CupcakeDetails
