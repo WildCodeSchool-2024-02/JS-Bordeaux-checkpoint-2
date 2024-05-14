92b5d73698fd0531fcc5e8aa004961361a57ac74
Rollet Fabien <fabrol47@gmail.com>
2024-05-14 12:37:23 +0200

    WIP step 5

--- client/src/pages/CupcakeList.jsx
+++ client/src/pages/CupcakeList.jsx
@@ -22,8 +22,16 @@ function CupcakeList() {
   console.info(accessoriesData)
 
   // Step 5: create filter state
+  const [selectedAccessories, setSelectedAccessories] = useState([]);
+  const handleSelectChange = (e) => {
+    console.info(e.target.value);
+    setSelectedAccessories(e.target.value);
+  }; 
 
+  const filtered = accessoriesData.filter((data) =>(data.slug[selectedAccessories]))
+  console.info(filtered);
 
+  // Stuck here :(
 
   return (
     <>
@@ -31,19 +39,22 @@ function CupcakeList() {
       <form className="center">
         <label htmlFor="cupcake-select">
           {/* Step 5: use a controlled component for select */}
+          
           Filter by{" "}
-          <select id="cupcake-select">
-            <option value="">---</option>
+          <select id="cupcake-select" onChange={handleSelectChange}>
+            selectaccessories
             {/* Step 4: add an option for each accessory */}
 
+            <option value="">---</option>
             {accessoriesData.map((data) => (
-          <option key={accessoriesData.id}>{data.name}</option>))}
-          
+          <option key={accessoriesData.id} value={data.name}>{data.name}</option>))}
+
           </select>
         </label>
       </form>
       <ul className="cupcake-list" id="cupcake-list">
         {/* Step 2: repeat this block for each cupcake */}
+        
         {cupcakeData.map((cupcake) => (
           <li key={cupcake.id}>
             <Cupcake data={cupcake} />
