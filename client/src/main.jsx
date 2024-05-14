67f05824486db10d9bf162593976e8ae0208d643
Yoann FORTIN <yoann.fortin.dev@gmail.com>
2024-05-14 15:54:11 +0200

    Bonus

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,6 +10,11 @@ import Instructions from "./pages/Instructions";
 import CupcakeList from "./pages/CupcakeList";
 import CupcakeDetails from "./pages/CupcakeDetails";
 
+const fetchApi = (url) =>  fetch(url)
+  .then(res => res.json())
+  .then(data => data)
+  .catch(err => console.error(err))
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -27,19 +32,13 @@ const router = createBrowserRouter([
         path: "/cupcakes",
         element: <CupcakeList />,
         // Step 1: load data here
-        loader: () => fetch("http://localhost:3310/api/cupcakes")
-          .then(result => result.json())
-          .then(data => data)
-          .catch(err => console.error(err))
+        loader: () => fetchApi("http://localhost:3310/api/cupcakes")
         ,
       },
       {
         path: "/cupcakes/:id",
         element: <CupcakeDetails />,
-        loader: ({ params }) => fetch(`http://localhost:3310/api/cupcakes/${params.id}`)
-        .then(res => res.json())
-        .then(data => data)
-        .catch(err => console.error(err))
+        loader: ({ params }) => fetchApi(`http://localhost:3310/api/cupcakes/${params.id}`)
       },
     ],
   },
